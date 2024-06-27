6f09b2659e4b5d528badddb6ba8ec084b94cd039
alpharii <alphari404@gmail.com>
2024-06-27 20:57:33 +0700

    first commit

---
+++ object.js
@@ -0,0 +1,34 @@
+//nama, tanggal lahir, status (precourse), batch(32), grade
+
+let nama = 'Bintang'
+let tanggalLahir = "21-11-2006"
+let status = "precourse"
+let batch = 32
+let grade = 2
+
+function peserta (hapus){
+    let result = {
+        nama: nama,
+        tanggalLahir: tanggalLahir,
+        status: status,
+        batch: batch,
+        grade: grade
+    }
+
+    if(hapus === 'nama'){
+        
+    } if(hapus === 'tanggalLahir'){
+        
+    } if(hapus === 'status'){
+        
+    }if(hapus === 'batch'){
+        
+    }if(hapus === 'grade'){
+       
+    }
+
+    return result
+}
+
+
+console.log(peserta('nama'))
